65c8003b2b94a9cbaf43e64dcea88cde98cbc950
Sagar Pamnani <sagarpamnani77@gmail.com>
2018-09-23 00:13:42 +0530

    Added Activities

--- app/src/main/java/com/project/imad/booksharingapp/GetStarted.java
+++ app/src/main/java/com/project/imad/booksharingapp/GetStarted.java
@@ -1,7 +1,10 @@
 package com.project.imad.booksharingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class GetStarted extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        Button start = (Button) findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GetStarted.this, Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/project/imad/booksharingapp/Login.java
+++ app/src/main/java/com/project/imad/booksharingapp/Login.java
@@ -1,7 +1,10 @@
 package com.project.imad.booksharingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this, Signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
